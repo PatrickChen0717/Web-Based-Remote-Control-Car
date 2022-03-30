7e9abeb5ef3af0174dd3836271c2168ac3ed4727
Eason1223 <fengguoyi1223@gmail.com>
2022-03-30 11:20:46 -0700

    sensor read

--- newProject.js
+++ newProject.js
@@ -1,14 +1,76 @@
-const express = require('express')
-const app = express()
-const port = 80
 
-app.use('/', express.static('public'));
+const res = require('express/lib/response');
+const port = 80;
+
+
 
 const fs = require('fs')
-var distance = 0;
+var distance1 = 0;
+var distance2 = 0;
+var distance3 = 0;
+var distance4 = 0;
+var express = require('express'); 
+var bodyParser = require('body-parser'); 
+var app = express(); 
 
 
-app.get('/newProject', function(req, res) {
+
+app.use('/', express.static('public'));
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: false })); 
+  
+app.post("/postdata", (req, res) => { 
+        
+    var left = req.body.leftdist; 
+    var right = req.body.rightdist; 
+    var front = req.body.frontdist; 
+    var back = req.body.backdist; 
+    console.log( left ,right ,front ,back);
+    
+    
+
+    fs.writeFile('./public/leftData.txt', String(left), err => {
+        if (err) {
+        console.error(err)
+        return
+        }
+        //file written successfully
+    })
+    fs.writeFile('./public/rightData.txt', String(right), err => {
+        if (err) {
+        console.error(err)
+        return
+        }
+        //file written successfully
+    })
+    fs.writeFile('./public/frontData.txt', String(front), err => {
+        if (err) {
+        console.error(err)
+        return
+        }
+        //file written successfully
+    })
+    fs.writeFile('./public/backData.txt', String(back), err => {
+        if (err) {
+        console.error(err)
+        return
+        }
+        //file written successfully
+    })
+    
+    res.send("process complete"); 
+}); 
+ 
+
+app.get("/getdata", (req, res) => { 
+    var data= { // this is the data you're sending back during the GET request 
+        direction: "forward",
+    } 
+    res.status(200).json(data) 
+}); 
+
+
+app.get('/left', function(req, res) {
 
     res.writeHead(200, {
         'Content-Type': 'text/event-stream',
@@ -23,12 +85,102 @@ app.get('/newProject', function(req, res) {
           
             // Converting Raw Buffer to text
             // data using tostring function.
-            distance = data;
+            distance1 = data;
             //console.log(data);
         })
 
         //console.log("SENT: "+distance);
-        res.write("data: " + distance + "\n\n")
+        res.write("data: " + distance1 + "\n\n")
+    }, 10);
+
+    // close
+    res.on('close', () => {
+        clearInterval(interval);
+        res.end();
+    });
+}) 
+
+app.get('/right', function(req, res) {
+
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    });
+
+    var interval = setInterval(function(){
+
+        fs.readFile('./public/rightData.txt', 'utf-8', (err, data) => {
+            //if (err) throw err;
+          
+            // Converting Raw Buffer to text
+            // data using tostring function.
+            distance2 = data;
+            //console.log(data);
+        })
+
+        //console.log("SENT: "+distance);
+        res.write("data: " + distance2 + "\n\n")
+    }, 10);
+
+    // close
+    res.on('close', () => {
+        clearInterval(interval);
+        res.end();
+    });
+}) 
+
+app.get('/front', function(req, res) {
+
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    });
+
+    var interval = setInterval(function(){
+
+        fs.readFile('./public/frontData.txt', 'utf-8', (err, data) => {
+            //if (err) throw err;
+          
+            // Converting Raw Buffer to text
+            // data using tostring function.
+            distance3 = data;
+            //console.log(data);
+        })
+
+        //console.log("SENT: "+distance);
+        res.write("data: " + distance3 + "\n\n")
+    }, 10);
+
+    // close
+    res.on('close', () => {
+        clearInterval(interval);
+        res.end();
+    });
+}) 
+
+app.get('/back', function(req, res) {
+
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    });
+
+    var interval = setInterval(function(){
+
+        fs.readFile('./public/backData.txt', 'utf-8', (err, data) => {
+            //if (err) throw err;
+          
+            // Converting Raw Buffer to text
+            // data using tostring function.
+            distance4 = data;
+            //console.log(data);
+        })
+
+        //console.log("SENT: "+distance);
+        res.write("data: " + distance4 + "\n\n")
     }, 10);
 
     // close
@@ -43,3 +195,5 @@ app.get('/newProject', function(req, res) {
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
 })
+
+ 
